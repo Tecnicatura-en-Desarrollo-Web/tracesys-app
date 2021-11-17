29c009ab1b79b1e7483b9b647cab88824dcf9a43
wtfranco22 <francofranco_96@hotmail.com>
2021-11-16 22:11:32 -0300

    agregando un poco de estilos al login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,44 +1,64 @@
 <template>
-  <ion-card>
-    <ion-card-header>
-      <ion-card-title class="ion-text-center">Iniciar sesión</ion-card-title>
-    </ion-card-header>
-    <ion-card-content>
-      <ion-item>
-        <ion-label position="floating">Usuario:</ion-label>
-        <ion-input v-model="usuario.email"></ion-input>
-      </ion-item>
-      <ion-item>
-        <ion-label position="floating">Contraseña:</ion-label>
-        <ion-input type="password" v-model="usuario.contrasena"></ion-input>
-      </ion-item>
-      <br />
-      <ion-button @click="iniciarSesion()">
-        Ingresar
-      </ion-button>
-    </ion-card-content>
-  </ion-card>
+  <ion-app>
+    <ion-header class="ion-padding">
+      <ion-toolbar>
+        <ion-title>
+          <ion-img :src="`/img/logoTracesysyChiquito.png`"> </ion-img>
+        </ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding" color="light">
+      <ion-card class="ion-padding">
+        <ion-card-content>
+          <ion-item>
+            <ion-label position="floating">Usuario:</ion-label>
+            <ion-input v-model="usuario.email"></ion-input>
+          </ion-item>
+          <ion-item>
+            <ion-label position="floating">Contraseña:</ion-label>
+            <ion-input type="password" v-model="usuario.contrasena"></ion-input>
+          </ion-item>
+          <ion-button
+            id="botonLogin"
+            shape="round"
+            color="primary"
+            @click="iniciarSesion()"
+          >
+            Ingresar
+          </ion-button>
+        </ion-card-content>
+      </ion-card>
+    </ion-content>
+  </ion-app>
 </template>
-
 <script>
 import {
+  IonApp,
+  IonToolbar,
+  IonHeader,
+  IonTitle,
+  IonContent,
+  IonImg,
   IonCard,
-  IonCardHeader,
-  IonCardTitle,
   IonItem,
   IonLabel,
   IonInput,
   IonButton,
   IonCardContent,
   alertController,
+  loadingController,
 } from "@ionic/vue";
 import { defineComponent } from "vue";
 import { mapGetters, mapActions } from "vuex";
 export default defineComponent({
   components: {
+    IonApp,
+    IonToolbar,
+    IonHeader,
+    IonTitle,
+    IonContent,
+    IonImg,
     IonCard,
-    IonCardHeader,
-    IonCardTitle,
     IonItem,
     IonLabel,
     IonInput,
@@ -50,6 +70,10 @@ export default defineComponent({
       usuario: {
         email: "",
         contrasena: "",
+      },
+      timeout: {
+        type: Number,
+        default: 5000,
       },
     };
   },
@@ -63,30 +87,37 @@ export default defineComponent({
       loginUser: "loginUser",
     }),
     async iniciarSesion() {
+      const cargando = await loadingController.create({
+        cssClass: "my-custom-class",
+        message: "Cargando...",
+        duration: this.timeout,
+      });
+      await cargando.present();
       if (this.usuario.email && this.usuario.contrasena) {
         await this.loginUser(this.usuario);
         if (this.loginStatus === "success") {
-          window.location.href = "http://localhost:8100/home";
+          window.location.href = "http://localhost:8100/lista-productos";
         } else {
           const datosIncorrectos = await alertController.create({
             cssClass: "my-custom-class",
             header: "Datos incorrectos!",
             subHeader: "Verifique los datos ingresados",
-            message:
-              "Ante cualquier inconveniente lo esperamos en nuestra sucursal mas cercana",
+            message: "Verifique su usuario/correo y contraseña",
             buttons: ["OK"],
           });
+          cargando.dismiss();
           await datosIncorrectos.present();
         }
       } else {
         const camposInvalidos = await alertController.create({
           cssClass: "my-custom-class",
-          header: "Datos no validos!",
+          header: "Datos vacíos",
+          animated: true,
           subHeader: "Campos vacíos",
-          message:
-            "Debe ingresar su correo y contraseña",
+          message: "Debe ingresar su correo y contraseña",
           buttons: ["OK"],
         });
+        cargando.dismiss();
         await camposInvalidos.present();
       }
     },
