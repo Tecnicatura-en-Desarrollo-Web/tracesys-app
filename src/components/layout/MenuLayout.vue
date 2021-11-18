a835bc831547c822c3d2991688e66f1c9846a09d
wtfranco22 <francofranco_96@hotmail.com>
2021-11-18 03:34:34 -0300

    se agregan nuevos componentes para el encabezado, pie de pagina y menu del sitio, agregando para completar la navegabilidad de la app

---
+++ src/components/layout/MenuLayout.vue
@@ -0,0 +1,78 @@
+<template>
+  <ion-menu side="start" content-id="menuOpciones" class="ion-text-center">
+    <ion-header>
+      <ion-toolbar color="primary">
+        <ion-title> Menu </ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <ion-list>
+        <ion-router-link color="dark" href="/">
+          <ion-item>
+            <ion-icon :src="notifications"> </ion-icon>
+            <ion-label class="ion-margin"> Productos </ion-label>
+          </ion-item>
+        </ion-router-link>
+        <ion-router-link color="dark" href="#">
+          <ion-item>
+            <ion-icon :src="person"></ion-icon>
+            <ion-label class="ion-margin"> Perfil </ion-label>
+          </ion-item>
+        </ion-router-link>
+      </ion-list>
+    </ion-content>
+    <ion-footer>
+      <ion-toolbar class="ion-text-capitalize">
+        <ion-icon :src="person"></ion-icon>
+          Cliente: {{ authData.userName }}
+      </ion-toolbar>
+    </ion-footer>
+  </ion-menu>
+  <ion-router-outlet id="menuOpciones"></ion-router-outlet>
+</template>
+
+<script>
+import {
+  IonMenu,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonIcon,
+  IonRouterOutlet,
+  IonFooter,
+} from "@ionic/vue";
+import { defineComponent } from "@vue/runtime-core";
+import { notifications, person } from "ionicons/icons";
+import { mapGetters } from "vuex";
+export default defineComponent({
+  props: ["tituloPagina", "enlacePaginaAnterior"],
+  components: {
+    IonMenu,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonIcon,
+    IonFooter,
+    IonRouterOutlet,
+  },
+  data() {
+    return {
+      person,
+      notifications,
+    };
+  },
+  computed: {
+    ...mapGetters("auth", {
+      authData: "getAuthData",
+    }),
+  },
+});
+</script>
