4b44653ee671e182b9f7eafcb387b45ffdb831b1
wtfranco22 <francofranco_96@hotmail.com>
2021-11-18 03:34:34 -0300

    se agregan nuevos componentes para el encabezado, pie de pagina y menu del sitio, agregando para completar la navegabilidad de la app

---
+++ src/components/layout/FooterLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <ion-footer>
+    <ion-toolbar>
+      <ion-title>
+          <ion-img :src="`/img/logoTracesysyChiquito.png`" id="imgFooter">
+          </ion-img>
+      </ion-title>
+    </ion-toolbar>
+  </ion-footer>
+</template>
+<style>
+ion-footer {
+  height: 10%;
+}
+#imgFooter {
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 65%;
+}
+</style>
+<script>
+import { IonFooter, IonToolbar, IonTitle, IonImg } from "@ionic/vue";
+
+export default {
+  components: {
+    IonFooter,
+    IonTitle,
+    IonToolbar,
+    IonImg,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ src/components/layout/HeaderLayout.vue
@@ -0,0 +1,77 @@
+<template>
+  <ion-app>
+    <ion-page >
+      <menu-layout></menu-layout>
+      <ion-header>
+        <ion-toolbar>
+          <ion-buttons slot="start">
+            <ion-menu-button id="menuOpciones">
+              <ion-icon :src="menuSharp"></ion-icon>
+            </ion-menu-button>
+          </ion-buttons>
+          <ion-title>
+            {{ tituloPagina }}
+          </ion-title>
+          <ion-buttons slot="end">
+            <ion-back-button :default-href="enlacePaginaAnterior">
+            </ion-back-button>
+          </ion-buttons>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content color="light" class="ion-padding">
+        <slot />
+      </ion-content>
+      <!-- <ion-footer> -->
+        <footer-layout></footer-layout>
+      <!-- </ion-footer> -->
+    </ion-page>
+  </ion-app>
+</template>
+<script>
+import {
+  IonApp,
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonMenuButton,
+  IonBackButton,
+  IonTitle,
+  IonContent,
+  IonIcon,
+  // IonFooter,
+} from "@ionic/vue";
+import { defineComponent } from "@vue/runtime-core";
+import { menuSharp } from "ionicons/icons";
+import MenuLayout from "@/components/layout/MenuLayout.vue";
+import FooterLayout from "@/components/layout/FooterLayout.vue";
+export default defineComponent({
+  props: ["tituloPagina", "enlacePaginaAnterior"],
+  components: {
+    IonApp,
+    IonPage,
+    IonHeader,
+    IonTitle,
+    IonButtons,
+    IonMenuButton,
+    IonBackButton,
+    IonToolbar,
+    IonContent,
+    IonIcon,
+    MenuLayout,
+    FooterLayout,
+    // IonFooter,
+  },
+  data() {
+    return {
+      menuSharp,
+    };
+  },
+});
+</script>
+<style>
+ion-icon,
+ion-back-button {
+  color: blue;
+}
+</style>
